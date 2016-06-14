cf206ee2facf9a26f043a7177b7cd42f3a016ce2
diffuser <n.y.prokopets@gmail.com>
2016-06-14 17:36:56 +0300

    Some syntax cleaning Raising failed connection errors (

--- diris_poller_b.py
+++ diris_poller_b.py
@@ -1,5 +1,6 @@
-#import subprocess
-import time
+# -*- coding: utf-8 -*- 
+
+import time, sys
 import datetime
 #from pymodbus.client.sync import ModbusTcpClient as ModbusClient
 from pymodbus.constants import Endian
@@ -7,57 +8,38 @@ from pymodbus.client.sync import ModbusSerialClient as ModbusClient
 from pymodbus.payload import BinaryPayloadDecoder
 
 
-diris_host = "127.0.0.1"
+
 diris_slave_id = 1
-diris_data = {'totalpower':802, 'realpwoer':803}
+
 data_file_prefix = 'diris_data_'
-time_lapse = 30
+time_lapse = 3
 diris_port='COM3'
 
-# diris_data['totalpower']
-
-'''
-import logging
-logging.basicConfig()
-log = logging.getLogger()
-log.setLevel(logging.DEBUG)
-'''
 
 
 def write_data(datastr):
 	now = datetime.datetime.now()
-	data_file_name = data_file_prefix + now.strftime("%Y-%m-%d") + ".log"
+	data_file_name = data_file_prefix + now.strftime("%Y-%m") + ".log"
 	OF = open(data_file_name,"a")
 	OF.write(datastr + "\n")
 	print datastr
 	OF.close()
-
-	#return data_file_name
-
-
-'''
-# [ k for k,v in diris_data.items()]
-
-rk = client.read_input_registers(0,5,unit=0x1)
-re = client.read_holding_registers(0,5,unit=0x1)
-print re
-print re.registers
-
-
-'''
 
 
 	
 if __name__  == "__main__":
 
 
-
 	now = datetime.datetime.now()
 	write_data("Starting data logger "+ now.strftime("%Y-%m-%d %H:%M")+"\n\n")
-
-#	diris_modbus = ModbusClient(diris_host)
+	
 	diris_modbus = ModbusClient(method='rtu', port=diris_port, timeout=1, baudrate=9600, parity='E')
-	diris_modbus.connect()
+	
+	if (not diris_modbus.connect()):
+		print "Connection Failed"
+		sys.exit()
+	
+	
 
 
 
@@ -70,16 +52,9 @@ if __name__  == "__main__":
 
 		rk = diris_modbus.read_holding_registers(50538,2,unit=diris_slave_id)
 		reactive_p = BinaryPayloadDecoder.fromRegisters(rk.registers,endian=Endian.Big).decode_32bit_uint()
-		reactive_p = float(reactive_p)/100
 
 		rk = diris_modbus.read_holding_registers(50540,2,unit=diris_slave_id)
-		apparent_p = BinaryPayloadDecoder.fromRegisters(rk.registers,endian=Endian.Big).decode_32bit_uint()
-		apparent_p = float(apparent_p)/100
-
-#		decoder = BinaryPayloadDecoder.fromRegisters(rk.registers,endian=Endian.Big)
-#		print decoder.decode_32bit_uint()
-
-
+		apparent_p = BinaryPayloadDecoder.fromRegisters(rk.registers,endian=Endian.Big).decode_32bit_int()
 
 		now = datetime.datetime.now()
 		result_string = '%s = (Kw, Var, KVA),%s,%s,%s' % (now.strftime("%H:%M:%S "),active_p,reactive_p,apparent_p)
